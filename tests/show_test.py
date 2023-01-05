a69380d6cf63017a849bc86f27a6416391816dd1
Jingwen Xie <jingwenxie@microsoft.com>
2023-01-05 06:19:31 +0000

    coverage

--- tests/show_test.py
+++ tests/show_test.py
@@ -3,12 +3,36 @@ import sys
 import show.main as show
 from click.testing import CliRunner
 from unittest import mock
-from unittest.mock import call
+from unittest.mock import call, MagicMock
 
 test_path = os.path.dirname(os.path.abspath(__file__))
 modules_path = os.path.dirname(test_path)
 sys.path.insert(0, test_path)
 sys.path.insert(0, modules_path)
+
+class TestCommonFunc(object):
+    @classmethod
+    def setup_class(cls):
+        print("SETUP")
+        os.environ["UTILITIES_UNIT_TESTING"] = "1"
+
+    def test_get_cmd_output(self):
+        with mock.patch('show.main.subprocess.Popen',
+                mock.MagicMock()) as mock_popen:
+            mock_proc = MagicMock()
+            mock_proc.communicate = MagicMock(return_value=(None, None))
+            mock_proc.returncode = 1
+            mock_popen.return_value = mock_proc
+            result = CliRunner().invoke(show.cli.commands['runningconfiguration'].commands['all'], [])
+        assert result.exit_code != 0
+        assert "Aborted" in result.output
+
+    @classmethod
+    def teardown_class(cls):
+        print("TEARDOWN")
+        os.environ["PATH"] = os.pathsep.join(os.environ["PATH"].split(os.pathsep)[:-1])
+        os.environ["UTILITIES_UNIT_TESTING"] = "0"
+
 
 class TestShowRunAllCommands(object):
     @classmethod
