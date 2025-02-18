750f317bf7ba0e36b6fed207ad4c0372e1faec2e
jingwenxie <jingwenxie@microsoft.com>
2025-02-18 07:03:11 +0000

    Revert "Enable multi asic golden config YANG validation. (#3685)"
    This reverts commit bf6ff9f921bd05fb09eddfd685b7256de8ad8f0e.

--- config/main.py
+++ config/main.py
@@ -1386,21 +1386,17 @@ def multiasic_write_to_db(filename, load_sysinfo):
 
 
 def config_file_yang_validation(filename):
-    config = read_json_file(filename)
+    config_to_check = read_json_file(filename)
     sy = sonic_yang.SonicYang(YANG_DIR)
     sy.loadYangModel()
-    asic_list = [HOST_NAMESPACE]
-    if multi_asic.is_multi_asic():
-        asic_list.extend(multi_asic.get_namespace_list())
-    for scope in asic_list:
-        config_to_check = config.get(scope) if multi_asic.is_multi_asic() else config
-        try:
-            sy.loadData(configdbJson=config_to_check)
-            sy.validate_data_tree()
-        except sonic_yang.SonicYangException as e:
-            click.secho("{} fails YANG validation! Error: {}".format(filename, str(e)),
-                        fg='magenta')
-            raise click.Abort()
+    try:
+        sy.loadData(configdbJson=config_to_check)
+        sy.validate_data_tree()
+    except sonic_yang.SonicYangException as e:
+        click.secho("{} fails YANG validation! Error: {}".format(filename, str(e)),
+                    fg='magenta')
+        raise click.Abort()
+
 
 # This is our main entrypoint - the main 'config' command
 @click.group(cls=clicommon.AbbreviationGroup, context_settings=CONTEXT_SETTINGS)
@@ -2036,18 +2032,21 @@ def load_minigraph(db, no_service_restart, traffic_shift_away, override_config, 
                         fg='magenta')
             raise click.Abort()
 
-        config_file_yang_validation(golden_config_path)
-
         config_to_check = read_json_file(golden_config_path)
-        # Dependency check golden config json
-        asic_list = [HOST_NAMESPACE]
         if multi_asic.is_multi_asic():
-            asic_list.extend(multi_asic.get_namespace_list())
-        for scope in asic_list:
-            host_config = config_to_check.get(scope) if multi_asic.is_multi_asic() else config_to_check
-            table_hard_dependency_check(host_config)
+            # Multiasic has not 100% fully validated. Thus pass here.
+            pass
+        else:
+            config_file_yang_validation(golden_config_path)
 
-    # Stop services before config push
+        # Dependency check golden config json
+        if multi_asic.is_multi_asic():
+            host_config = config_to_check.get('localhost', {})
+        else:
+            host_config = config_to_check
+        table_hard_dependency_check(host_config)
+
+    #Stop services before config push
     if not no_service_restart:
         log.log_notice("'load_minigraph' stopping services...")
         delete_transceiver_tables()
